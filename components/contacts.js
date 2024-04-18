2e00c6732a9d70540ef77e2a9043dac5d98d914f
Teamofeyy <128955381+Teamofeyy@users.noreply.github.com>
2024-04-18 12:36:22 +0300

    changes

--- components/contacts.js
+++ components/contacts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { gStyle } from '..npz /Styles/style';
+import { gStyle } from '../Styles/style';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
